833e55a4239652fd3256560086f1995d96d82910
thng292 <nguyenquangthong292@gmail.com>
2022-11-10 12:29:28 +0700

    Add dist directory

---
+++ dist/assets/CheckOut.8a115b82.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.dcd18b05.js";function n(){return e("div",{style:{display:"flex",justifyContent:"center",alignItems:"center"},children:e("p",{style:{fontFamily:"sans-serif",fontWeight:"bolder",fontSize:"4rem"},children:"CheckOut"})})}export{n as default};
