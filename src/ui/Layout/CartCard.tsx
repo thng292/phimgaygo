905fa2a0aba6d5b70bb17a474df362a22cf8a862
thng292 <nguyenquangthong292@gmail.com>
2022-11-22 14:36:51 +0700

    Redeploy to Vercel

--- src/ui/Layout/CartCard.tsx
+++ src/ui/Layout/CartCard.tsx
@@ -1,30 +1,31 @@
-import React, { FC } from "react"
-import config from "../../data/datasource/config"
-import FilmOverview from "../../data/model/FilmOverview"
-import Cart from "./Cart"
+// import React, { FC } from "react"
+// import config from "../../data/datasource/config"
+// import FilmOverview from "../../data/model/FilmOverview"
 
-const CartCard: FC<{
-    cart: Cart<FilmOverview>,
-    onClose: () => void,
-}> = ({ cart }) => {
-    let cartItem = cart.getItems()
-    return <div className="cart-card">
-        {cartItem.map((item, index) => {
-            return <>
-                <div className="row">
-                    <img
-                        src={config.posterUrl + item.mainItem.poster_path}
-                        alt={item.mainItem.title}
-                    />
-                    <div>
-                        <p className="p10">{item.mainItem.title}</p>
-                        <p className="p10">{ item.price * item.quantity }</p>
-                        <p className="p10">{ item.quantity }</p>
-                    </div>
-                </div>
-            </>
-        })}
-    </div>
-}
+// const CartCard: FC<{
+//     cart: Cart<FilmOverview>,
+//     onClose: () => void,
+// }> = ({ cart }) => {
+//     let cartItem = cart.getItems()
+//     return <div className="cart-card">
+//         {cartItem.map((item, index) => {
+//             return <>
+//                 <div className="row">
+//                     <img
+//                         src={config.posterUrl + item.mainItem.poster_path}
+//                         alt={item.mainItem.title}
+//                     />
+//                     <div>
+//                         <p className="p10">{item.mainItem.title}</p>
+//                         <p className="p10">{ item.price * item.quantity }</p>
+//                         <p className="p10">{ item.quantity }</p>
+//                     </div>
+//                 </div>
+//             </>
+//         })}
+//     </div>
+// }
 
-export default CartCard;
+// export default CartCard;
+
+export default {}
